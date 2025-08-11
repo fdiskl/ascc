1536f01b7fc6420a00d5b4ad8316777956852030
fdiskl <stepanovsm8@gmail.com>
2025-08-11 13:05:44 +0300

    feat: make main invoke driver

--- src/main.c
+++ src/main.c
@@ -1,26 +1,32 @@
 #include "arena.h"
+#include "driver.h"
 #include "scan.h"
+#include <assert.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
   init_arena(&str_arena);
 
-  // TODO: driver
-  FILE *in_file = fopen("test.c", "r");
+  driver_options opts;
+
+  parse_driver_options(&opts, argc, argv);
+
+  FILE *in_file = fopen(opts.input, "r");
 
   lexer l;
   init_lexer(&l, in_file);
 
-  // TODO: only if --lex is set
-  if (1) {
+  if (opts.dof == DOF_LEX) {
     token t;
     do {
       next(&l, &t);
       print_token(&t);
     } while (t.token != TOK_EOF);
+    return 0;
   }
 
-  free_lexer(&l);
+  assert(0 && "todo");
 
+  free_lexer(&l);
   free_arena(&str_arena);
 }
