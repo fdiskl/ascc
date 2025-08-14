367d33bb0ea293173f275584d574486ad5ec6b80
aaa <stepanovsm8@gmail.com>
2025-08-14 17:15:56 +0300

    add: 'todo' macro

--- src/common.h
+++ src/common.h
@@ -13,6 +13,9 @@
   fprintf(stderr, "unreachable code reached (file: %s, line: %d)", __FILE__,   \
           __LINE__)
 
+#define todo()                                                                 \
+  fprintf(stderr, "not implemented yet (file: %s, line %d)", __FILE__, __LINE__)
+
 void after_error();
 
 #endif
