4b2db00e73ac49d13a4853dcce3938cfffbc3202
aaa <stepanovsm8@gmail.com>
2025-08-14 17:20:55 +0300

    fix: unreachable and todo macros

--- src/common.h
+++ src/common.h
@@ -1,20 +1,26 @@
 #ifndef _ASCC_COMMON_H
 #define _ASCC_COMMON_H
 
-#include "stdio.h"
 #include <stdalign.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // If "DEBUG_INFO" is defined, compiler will emit extra debug info
 #define DEBUG_INFO
 
 #define unreachable()                                                          \
   fprintf(stderr, "unreachable code reached (file: %s, line: %d)", __FILE__,   \
-          __LINE__)
+          __LINE__);                                                           \
+  after_error();                                                               \
+  exit(1);
 
 #define todo()                                                                 \
-  fprintf(stderr, "not implemented yet (file: %s, line %d)", __FILE__, __LINE__)
+  fprintf(stderr, "not implemented yet (file: %s, line %d)", __FILE__,         \
+          __LINE__);                                                           \
+  after_error();                                                               \
+  exit(1);
 
 void after_error();
 
