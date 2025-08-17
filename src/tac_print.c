2ad109a9dd304de0963e70358ff395020ec61bdb
aaa <stepanovsm8@gmail.com>
2025-08-17 18:16:53 +0300

    chore: update tac_print

--- src/tac_print.c
+++ src/tac_print.c
@@ -6,18 +6,27 @@ const char *tacop_str(tacop op) {
   case TAC_RET:
     return "ret";
   case TAC_COMPLEMENT:
-    return "complement";
+    return "~";
   case TAC_NEGATE:
-    return "negate";
-  default:
-    unreachable();
+    return "-";
+  case TAC_ADD:
+    return "+";
+  case TAC_SUB:
+    return "-";
+  case TAC_MUL:
+    return "*";
+  case TAC_DIV:
+    return "/";
+  case TAC_MOD:
+    return "%";
+    break;
   }
 }
 
 static void print_val(tacv *v) {
   switch (v->t) {
   case TACV_CONST:
-    printf("#%llu", (unsigned long long)v->intv);
+    printf("%llu", (unsigned long long)v->intv);
     break;
   case TACV_VAR:
     printf("v%d", v->var_idx);
@@ -27,27 +36,42 @@ static void print_val(tacv *v) {
   }
 }
 
-static void print_unary(tacv *dst, tacv *src) {
+static void print_unary(tacv *dst, tacv *src, const char *ops) {
   print_val(dst);
-  printf(", ");
+  printf(" = %s", ops);
   print_val(src);
+}
+
+static void print_binary(tacv *dst, tacv *src1, tacv *src2, const char *ops) {
+  print_val(dst);
+  printf(" = ");
+  print_val(src1);
+  printf(" %s ", ops);
+  print_val(src2);
 }
 
 void print_tac_func(tacf *f) {
   printf("func %s:\n", f->name);
 
   for (taci *i = f->firsti; i != NULL; i = i->next) {
-    printf("    %-6s ", tacop_str(i->op));
+    printf("\t");
 
     switch (i->op) {
     case TAC_RET:
+      printf("%s ", tacop_str(i->op));
       print_val(&i->src1);
       break;
     case TAC_COMPLEMENT:
     case TAC_NEGATE:
-      print_unary(&i->dst, &i->src1);
-    default:
-      unreachable();
+      print_unary(&i->dst, &i->src1, tacop_str(i->op));
+      break;
+    case TAC_ADD:
+    case TAC_SUB:
+    case TAC_MUL:
+    case TAC_DIV:
+    case TAC_MOD:
+      print_binary(&i->dst, &i->src1, &i->src2, tacop_str(i->op));
+      break;
     }
 
     printf("\n");
