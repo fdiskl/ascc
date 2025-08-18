82e9e8c7015ccbb3bfd795172f3d572feb2c6a2a
aaa <stepanovsm8@gmail.com>
2025-08-18 10:40:08 +0300

    update: x86_fix_instrs.c to work with new instructions

--- src/x86_fix_instrs.c
+++ src/x86_fix_instrs.c
@@ -11,6 +11,14 @@ static x86_op new_r10() {
   return op;
 }
 
+static x86_op new_r11() {
+  x86_op op;
+  op.t = X86_OP_REG;
+  op.v.reg.t = X86_R11;
+  op.v.reg.size = 4;
+  return op;
+}
+
 // defined in x86.c
 x86_instr *alloc_x86_instr(x86_asm_gen *ag, int op);
 
@@ -28,19 +36,43 @@ static void insert_after_x86_instr(x86_instr *i, x86_instr *new) {
   i->next = new;
 }
 
-static void fix_mov(x86_asm_gen *ag, x86_instr *mov) {
-  assert(mov->op == X86_MOV);
+static void fix_mov_add_sub(x86_asm_gen *ag, x86_instr *i) {
+  assert(i->op == X86_MOV || i->op == X86_ADD || i->op == X86_SUB);
 
-  if (mov->v.binary.dst.t == X86_OP_STACK &&
-      mov->v.binary.src.t == X86_OP_STACK) {
+  if (i->v.binary.dst.t == X86_OP_STACK && i->v.binary.src.t == X86_OP_STACK) {
 
-    x86_instr *new_mov = alloc_x86_instr(ag, X86_MOV);
-    new_mov->v.binary.src = new_r10();
-    new_mov->v.binary.dst = mov->v.binary.dst;
+    x86_instr *mov = alloc_x86_instr(ag, X86_MOV);
+    mov->v.binary.src = i->v.binary.src;
+    i->v.binary.src = mov->v.binary.dst = new_r10();
 
-    mov->v.binary.dst = new_r10();
+    insert_before_x86_instr(i, mov);
+  }
+}
 
-    insert_after_x86_instr(mov, new_mov);
+static void fix_idiv(x86_asm_gen *ag, x86_instr *i) {
+  assert(i->op == X86_IDIV);
+
+  if (i->v.unary.src.t == X86_OP_IMM) {
+    x86_instr *mov = alloc_x86_instr(ag, X86_MOV);
+    mov->v.binary.src = i->v.unary.src;
+    i->v.unary.src = mov->v.binary.dst = new_r11();
+    insert_before_x86_instr(i, mov);
+  }
+}
+
+static void fix_mult(x86_asm_gen *ag, x86_instr *i) {
+  assert(i->op == X86_MULT);
+
+  if (i->v.binary.dst.t == X86_OP_STACK) {
+    x86_instr *mov_before = alloc_x86_instr(ag, X86_MOV);
+    x86_instr *mov_after = alloc_x86_instr(ag, X86_MOV);
+
+    mov_after->v.binary.dst = mov_before->v.binary.src = i->v.binary.dst;
+    i->v.binary.dst = mov_after->v.binary.src = mov_before->v.binary.dst =
+        new_r11();
+
+    insert_before_x86_instr(i, mov_before);
+    insert_after_x86_instr(i, mov_after);
   }
 }
 
@@ -49,10 +81,19 @@ static void fix_instr(x86_asm_gen *ag, x86_instr *i) {
   case X86_RET:
   case X86_NOT:
   case X86_NEG:
+  case X86_CDQ:
   case X86_ALLOC_STACK:
     break;
   case X86_MOV:
-    fix_mov(ag, i);
+  case X86_ADD:
+  case X86_SUB:
+    fix_mov_add_sub(ag, i);
+    break;
+  case X86_IDIV:
+    fix_idiv(ag, i);
+    break;
+  case X86_MULT:
+    fix_mult(ag, i);
     break;
   }
 }
