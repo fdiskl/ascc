a6b1bd4de6530075b08d89688c79b17668cc8341
aaa <stepanovsm8@gmail.com>
2025-08-19 23:17:32 +0300

    feat: update x86 fix instrs phase to support new instrs

--- src/x86_fix_instrs.c
+++ src/x86_fix_instrs.c
@@ -2,6 +2,9 @@
 #include "common.h"
 #include "x86.h"
 #include <assert.h>
+#include <stdio.h>
+
+static void fix_instr(x86_asm_gen *ag, x86_instr *i);
 
 static x86_op new_r10() {
   x86_op op;
@@ -19,33 +22,53 @@ static x86_op new_r11() {
   return op;
 }
 
+static x86_op new_cl() {
+  x86_op op;
+  op.t = X86_OP_REG;
+  op.v.reg.t = X86_CX;
+  op.v.reg.size = 1;
+  return op;
+}
+
+static x86_op new_cx() {
+  x86_op op;
+  op.t = X86_OP_REG;
+  op.v.reg.t = X86_CX;
+  op.v.reg.size = 4;
+  return op;
+}
+
 // defined in x86.c
 x86_instr *alloc_x86_instr(x86_asm_gen *ag, int op);
 
-static void insert_before_x86_instr(x86_instr *i, x86_instr *new) {
+static void insert_before_x86_instr(x86_asm_gen *ag, x86_instr *i,
+                                    x86_instr *new) {
 
   if (i->prev == NULL)
     unreachable(); // at least alloc_stack shoud be prev
+
+  fix_instr(ag, new);
 
   new->next = i;
   i->prev->next = new;
 }
 
-static void insert_after_x86_instr(x86_instr *i, x86_instr *new) {
+static void insert_after_x86_instr(x86_asm_gen *ag, x86_instr *i,
+                                   x86_instr *new) {
+  fix_instr(ag, new);
+
   new->next = i->next;
   i->next = new;
 }
 
-static void fix_mov_add_sub(x86_asm_gen *ag, x86_instr *i) {
-  assert(i->op == X86_MOV || i->op == X86_ADD || i->op == X86_SUB);
-
+static void fix_both_ops_mem(x86_asm_gen *ag, x86_instr *i) {
   if (i->v.binary.dst.t == X86_OP_STACK && i->v.binary.src.t == X86_OP_STACK) {
 
     x86_instr *mov = alloc_x86_instr(ag, X86_MOV);
     mov->v.binary.src = i->v.binary.src;
     i->v.binary.src = mov->v.binary.dst = new_r10();
 
-    insert_before_x86_instr(i, mov);
+    insert_before_x86_instr(ag, i, mov);
   }
 }
 
@@ -56,7 +79,7 @@ static void fix_idiv(x86_asm_gen *ag, x86_instr *i) {
     x86_instr *mov = alloc_x86_instr(ag, X86_MOV);
     mov->v.binary.src = i->v.unary.src;
     i->v.unary.src = mov->v.binary.dst = new_r11();
-    insert_before_x86_instr(i, mov);
+    insert_before_x86_instr(ag, i, mov);
   }
 }
 
@@ -71,8 +94,24 @@ static void fix_mult(x86_asm_gen *ag, x86_instr *i) {
     i->v.binary.dst = mov_after->v.binary.src = mov_before->v.binary.dst =
         new_r11();
 
-    insert_before_x86_instr(i, mov_before);
-    insert_after_x86_instr(i, mov_after);
+    insert_before_x86_instr(ag, i, mov_before);
+    insert_after_x86_instr(ag, i, mov_after);
+  }
+}
+
+static void fix_shifts(x86_asm_gen *ag, x86_instr *i) {
+  assert(i->op == X86_SAR || i->op == X86_SHL);
+
+  // if not imm or cl
+  if (i->v.binary.src.t != X86_OP_IMM &&
+      !(i->v.binary.src.t == X86_OP_REG && i->v.binary.src.v.reg.t == X86_CX &&
+        i->v.binary.src.v.reg.size == 1)) {
+    x86_instr *mov = alloc_x86_instr(ag, X86_MOV);
+    mov->v.binary.src = i->v.binary.src;
+    mov->v.binary.dst = new_cx(); // should depend on size in future, for now
+                                  // it's always ints so 4 bytes
+    i->v.binary.src = new_cl();
+    insert_before_x86_instr(ag, i, mov);
   }
 }
 
@@ -87,13 +126,20 @@ static void fix_instr(x86_asm_gen *ag, x86_instr *i) {
   case X86_MOV:
   case X86_ADD:
   case X86_SUB:
-    fix_mov_add_sub(ag, i);
+  case X86_AND:
+  case X86_OR:
+  case X86_XOR:
+    fix_both_ops_mem(ag, i);
     break;
   case X86_IDIV:
     fix_idiv(ag, i);
     break;
   case X86_MULT:
     fix_mult(ag, i);
+    break;
+  case X86_SHL:
+  case X86_SAR:
+    fix_shifts(ag, i);
     break;
   }
 }
