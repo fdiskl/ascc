7e813dcfd0af05d02261c8492db863df3dbd98c5
aaa <stepanovsm8@gmail.com>
2025-08-29 21:00:08 +0300

    feat: add transform from assign div, assign mod to x86

--- src/x86.c
+++ src/x86.c
@@ -255,7 +255,18 @@ static void gen_asm_from_inc_dec(x86_asm_gen *ag, taci *i) {
 
 static void gen_asm_from_assign(x86_asm_gen *ag, taci *i) {
   if (i->op == TAC_ASDIV || i->op == TAC_ASMOD) {
-    TODO();
+    x86_instr *mov1 = insert_x86_instr(ag, X86_MOV, i);
+    insert_x86_instr(ag, X86_CDQ, i);
+    x86_instr *idiv = insert_x86_instr(ag, X86_IDIV, i);
+    x86_instr *mov2 = insert_x86_instr(ag, X86_MOV, i);
+
+    mov1->v.binary.dst = new_x86_reg(X86_AX);
+    mov2->v.binary.dst = mov1->v.binary.src = operand_from_tac_val(i->dst);
+
+    idiv->v.unary.src = operand_from_tac_val(i->src1);
+
+    mov2->v.binary.src = new_x86_reg(i->op == TAC_ASDIV ? X86_AX : X86_DX);
+    return;
   }
 
   int op;
